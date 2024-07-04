264060257bb9c55dab21b29e1834bb9a75d0d303
Mimohshukla00 <mimohshukla0001@gmail.com>
2024-07-04 23:22:50 +0530

    delete the files

--- dataTypes.java
+++
@@ -1,9 +0,0 @@
-public class dataTypes{
-    public static void main(String args[]) {
-        // int age =10;
-        byte b =8;
-        System.out.println(b);
-        // System.out.println(age);
-       
-    }
-}
